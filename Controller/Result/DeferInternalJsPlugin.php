686763855133c3ebaa8d8c194f7cc1b648a29455
Elisei <brunoelisei@o2ti.com>
2023-07-06 09:54:48 -0300

    Fixes PHPMD Reports

--- Controller/Result/DeferInternalJsPlugin.php
+++ Controller/Result/DeferInternalJsPlugin.php
@@ -1,83 +1,42 @@
 <?php
 /*
- *  Copyright Pure Mashiro. All rights reserved.
- *  @author Mana Light
+ * Copyright Pure Mashiro. All rights reserved.
+ * @author Mana Light
  */
 
 declare(strict_types=1);
 
-namespace PureMashiro\BundleJs\Plugin\Magento\RequireJs\Block\Html\Head;
+namespace PureMashiro\BundleJs\Controller\Result;
 
-use Magento\Framework\App\RequestInterface;
-use Magento\Framework\Filesystem\Driver\File as FileDriver;
-use Magento\Framework\RequireJs\Config as RequireJsConfig;
-use Magento\Framework\View\Asset\ConfigInterface as AssetConfig;
-use Magento\Framework\View\Asset\GroupedCollection;
-use Magento\Framework\View\Asset\Minification;
-use Magento\Framework\View\Page\Config as PageConfig;
-use Magento\RequireJs\Block\Html\Head\Config as HeadConfig;
-use Magento\Theme\Model\View\Design;
-use PureMashiro\BundleJs\Action\GenerateCriticalJsAssets;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\DataObject;
+use Magento\Framework\View\Result\Layout;
 use PureMashiro\BundleJs\Helper\Config as ConfigHelper;
-use PureMashiro\BundleJs\Model\BundleByType;
-use PureMashiro\BundleJs\Model\FileManager;
-use PureMashiro\BundleJs\Model\TypeMapper;
+use PureMashiro\BundleJs\Helper\DeferJsReplacer as HelperDeferJsReplacer;
+use PureMashiro\BundleJs\Model\DeferJsReplacer;
 use PureMashiro\BundleJs\Model\Validator\IsAllowedStaticPage;
 
-/**
- * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
- */
-class ConfigPlugin
+class DeferInternalJsPlugin
 {
-    /**
-     * @var FileManager
-     */
-    private $fileManager;
-
-    /**
-     * @var PageConfig
-     */
-    private $pageConfig;
-
-    /**
-     * @var Minification
-     */
-    private $minification;
-
     /**
      * @var ConfigHelper
      */
     private $configHelper;
 
     /**
-     * @var AssetConfig
+     * @var DeferJsReplacer
      */
-    private $assetConfig;
+    private $deferJsReplacer;
 
     /**
-     * @var GenerateCriticalJsAssets
+     * @var DataObject
      */
-    private $generateCriticaL;
+    private $dataObject;
 
     /**
-     * @var Design
+     * @var HelperDeferJsReplacer
      */
-    private $design;
-
-    /**
-     * @var FileDriver
-     */
-    private $fileDriver;
-
-    /**
-     * @var RequestInterface
-     */
-    private $request;
-
-    /**
-     * @var TypeMapper
-     */
-    private $typeMapper;
+    private $helperDeferJs;
 
     /**
      * @var IsAllowedStaticPage
@@ -85,164 +44,49 @@ class ConfigPlugin
     private $isAllowedStaticPage;
 
     /**
-     * @param FileManager $fileManager
-     * @param PageConfig $pageConfig
-     * @param Minification $minification
-     * @param ConfigHelper $configHelper
-     * @param AssetConfig $assetConfig
-     * @param GenerateCriticalJsAssets $generateCriticaL
-     * @param Design $design
-     * @param FileDriver $fileDriver
-     * @param RequestInterface $request
-     * @param TypeMapper $typeMapper
-     * @param IsAllowedStaticPage $isAllowedStaticPage
+     * Construct.
      *
-     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     * @param ConfigHelper $configHelper
+     * @param DeferJsReplacer $deferJsReplacer
+     * @param DataObject $dataObject
+     * @param HelperDeferJsReplacer $helperDeferJs
+     * @param IsAllowedStaticPage $isAllowedStaticPage
      */
     public function __construct(
-        FileManager              $fileManager,
-        PageConfig               $pageConfig,
-        Minification             $minification,
-        ConfigHelper             $configHelper,
-        AssetConfig              $assetConfig,
-        GenerateCriticalJsAssets $generateCriticaL,
-        Design                   $design,
-        FileDriver               $fileDriver,
-        RequestInterface         $request,
-        TypeMapper               $typeMapper,
-        IsAllowedStaticPage      $isAllowedStaticPage
+        ConfigHelper          $configHelper,
+        DeferJsReplacer       $deferJsReplacer,
+        DataObject            $dataObject,
+        HelperDeferJsReplacer $helperDeferJs,
+        IsAllowedStaticPage   $isAllowedStaticPage
     ) {
-        $this->fileManager = $fileManager;
-        $this->pageConfig = $pageConfig;
-        $this->minification = $minification;
         $this->configHelper = $configHelper;
-        $this->assetConfig = $assetConfig;
-        $this->generateCriticaL = $generateCriticaL;
-        $this->design = $design;
-        $this->fileDriver = $fileDriver;
-        $this->request = $request;
-        $this->typeMapper = $typeMapper;
+        $this->deferJsReplacer = $deferJsReplacer;
+        $this->dataObject = $dataObject;
+        $this->helperDeferJs = $helperDeferJs;
         $this->isAllowedStaticPage = $isAllowedStaticPage;
     }
 
     /**
-     * Set After Layout
+     * Set After Render Result.
      *
-     * @param HeadConfig $subject
-     * @param mixed $result
-     * @return mixed
-     *
-     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * @param Layout $subject
+     * @param Layout $result
+     * @param ResponseInterface $httpResponse
+     * @return Layout
      */
-    public function afterSetLayout(HeadConfig $subject, $result)
+    public function afterRenderResult(Layout $subject, Layout $result, ResponseInterface $httpResponse)
     {
-        if (!$this->isEnable()) {
+        if (!$this->configHelper->canDeferBundleJs()) {
             return $result;
         }
 
-        $after = RequireJsConfig::REQUIRE_JS_FILE_NAME;
-        if ($this->minification->isEnabled('js')) {
-            $minResolver = $this->fileManager->createMinResolverAsset();
-            $after = $minResolver->getFilePath();
-        }
-        $requireJsMapConfig = $this->fileManager->createRequireJsMapConfigAsset();
-        if ($requireJsMapConfig) {
-            $after = $requireJsMapConfig->getFilePath();
-        }
-
-        $assetCollection = $this->pageConfig->getAssetCollection();
-
-        if ($this->configHelper->isDisableBundlesOnStaticPages()
-            && $this->isAllowedStaticPage->validate($subject->getLayout())) {
-            $this->insertCriticalJsAssets($assetCollection, $after);
-        }
-        
-        if (!$this->configHelper->isDisableBundlesOnStaticPages()
-        && !$this->isAllowedStaticPage->validate($subject->getLayout())) {
-            $bundleAssets = $this->fileManager->createBundleJsPool();
-            $staticAsset = $this->fileManager->createStaticJsAsset();
-            /** @var \Magento\Framework\View\Asset\File $bundleAsset */
-            if (!empty($bundleAssets) && $staticAsset !== false) {
-                $bundleAssets = array_reverse($bundleAssets);
-                foreach ($bundleAssets as $bundleAsset) {
-                    $assetCollection->insert(
-                        $bundleAsset->getFilePath(),
-                        $bundleAsset,
-                        $after
-                    );
-                }
-
-                if ($this->isAllowedStaticPage->validate($subject->getLayout())) {
-                    $after = reset($bundleAssets)->getFilePath();
-                    $this->insertCriticalJsAssets($assetCollection, $after);
-                }
-
-                $assetCollection->insert(
-                    $staticAsset->getFilePath(),
-                    $staticAsset,
-                    reset($bundleAssets)->getFilePath()
-                );
-            }
+        if ($this->isAllowedStaticPage->validate($subject->getLayout())) {
+            $content = (string)$httpResponse->getContent();
+            $this->dataObject->setHelperDeferJsReplacer($this->helperDeferJs);
+            $dom = $this->deferJsReplacer->replaceHtml($content, $this->dataObject);
+            $httpResponse->setContent(is_string($dom) ? $dom : $dom->save());
         }
 
         return $result;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isEnable(): bool
-    {
-        return $this->configHelper->canBundleJsInStorefront() && !$this->assetConfig->isBundlingJsFiles();
-    }
-
-    /**
-     * @param GroupedCollection $assetCollection
-     * @param $after
-     * @return void
-     * @throws \Magento\Framework\Exception\FileSystemException
-     * @throws \Magento\Framework\Exception\LocalizedException
-     */
-    private function insertCriticalJsAssets(GroupedCollection $assetCollection, $after)
-    {
-        /**
-         * Reference \PureMashiro\BundleJs\Action\GetBundleTypes::execute
-         */
-        $fullActionName = $this->request->getFullActionName();
-        $pathInfo = $this->request->getOriginalPathInfo();
-        $type = $this->typeMapper->map($fullActionName, $pathInfo);
-        $fileTypes = empty($type) ? [BundleByType::TYPE_COMMON] : [BundleByType::TYPE_COMMON, $type];
-
-        $files = ['contexts_config.min.js'];
-
-        $allowedJsComponents = $this->configHelper->getAllowedJsComponents();
-        if (!empty($allowedJsComponents)) {
-            $files[] = 'allowed_components.min.js';
-        }
-
-        foreach ($fileTypes as $fileType) {
-            $files[] = "critical_$fileType.min.js";
-        }
-
-        $files = array_reverse($files);
-
-        $designParams = $this->design->getDesignParams();
-        $area = $designParams['area'];
-        $theme = $designParams['themeModel']->getThemePath();
-        $locale = $designParams['locale'];
-
-        foreach ($files as $filePath) {
-            $destination = $this->generateCriticaL->getFileDestination(false, $area, $theme, $locale, $filePath);
-            if (!$this->fileDriver->isExists($destination)) {
-                continue;
-            }
-
-            $criticalAsset = $this->fileManager->getCriticalJsAsset($filePath);
-            $assetCollection->insert(
-                $criticalAsset->getFilePath(),
-                $criticalAsset,
-                $after
-            );
-        }
     }
 }
